c75a6d110e7cc9350dcfaf156c8110d59b37a6fd
Gabriele Picco <piccogabriele@gmail.com>
2026-04-08 09:53:21 +0200

    Add logo

---
+++ postinstall.mjs
@@ -0,0 +1,15 @@
+const CYAN = "\x1b[36m";
+const RESET = "\x1b[0m";
+
+console.log(CYAN);
+console.log(`
+            ✦
+          ◢   ◣
+            ○
+          ◥   ◤
+            │
+           ▼
+
+          Mirage
+`);
+console.log(RESET);
